6b0119180dac7b3c361213757d4bc7d03b77cac7
Allan0411 <allan04dasilva@gmail.com>
2025-07-31 23:48:39 +0530

    added stockmovement and currentStock model codes

---
+++ src/models/currentStock.model.js
@@ -0,0 +1,71 @@
+const query=require('../db/db-connection');
+const {multipleColumnSet}=require('../utils/common.utils');
+
+class CurrentStockModel{
+    tableName='current_stock';
+
+    //find 
+    find=async (params={})=>{
+        let sql=`SELECT * FROM ${this.tableName}`;
+
+        if(!Object.keys(params).length){
+            return await query(sql);
+        }
+
+        const {columnSet,values}=multipleColumnSet(params);
+
+        sql+=` WHERE ${columnSet}`;
+
+        return await query(sql,values);
+
+    };
+
+    //find one
+    findOne=async(params)=>{
+        const {columnSet,values}=multipleColumnSet(params);
+
+        const sql=`SELECT * FROM ${this.tableName} WHERE ${columnSet} LIMIT 1`;
+
+        const result = await query(sql,values);
+        return result[0];
+    };
+
+    //create
+
+    create = async ({
+        product_id,
+        region_id,
+        quantity
+    }) => {
+        const sql = `INSERT INTO ${this.tableName} (product_id, region_id, quantity) VALUES (?, ?, ?)`;
+        const result = await query(sql, [product_id, region_id, quantity]);
+        console.log("affected rows:", result?.affectedRows);
+        return result ? result.affectedRows : 0;
+    };
+
+    //update
+    update = async (params, product_id, region_id) => {
+        const { columnSet, values } = multipleColumnSet(params);
+
+        // There is a typo in the original SQL: "region id" should be "region_id"
+        const sql = `UPDATE ${this.tableName} SET ${columnSet} WHERE product_id = ? AND region_id = ?`;
+
+        const result = await query(sql, [...values, product_id, region_id]);
+
+        return result;
+    };
+    //delete
+    
+    delete=async(product_id,region_id)=>{
+
+        const sql=`DELETE FROM ${this.tableName} WHERE product_id=? and region_id=?`;
+        const result= await query(sql,[product_id,region_id]);
+        return result;
+    };
+
+
+
+};
+
+
+module.exports=new CurrentStockModel();
---
+++ src/models/stockMovement.model.js
@@ -0,0 +1,56 @@
+const query=require('../db/db-connection.js');
+const {multipleColumnSet}=require('../utils/common.utils');
+
+class StockMovementModel{
+    tableName='stockmovement';
+
+    
+    //find
+    find=async (params={})=>{
+        let sql=`SELECT * FROM ${this.tableName}`;
+        if (!Object.keys(params).length){
+            return await query(sql);
+        }
+
+        const {columnSet,values}=multipleColumnSet(params);
+        sql+=` Where ${columnSet}`;
+
+        return await query(sql,values);
+    }
+
+    
+
+    //create
+    create = async ({
+        id,
+        product_id,
+        region_id,
+        user_id,
+        change_in_stock,
+        type,
+        note
+      }) => {
+        const sql = `
+          INSERT INTO ${this.tableName}
+          (id, product_id, region_id, user_id, change_in_stock, type, note)
+          VALUES (?, ?, ?, ?, ?, ?, ?)
+        `;
+    
+        const result = await query(sql, [
+          id,
+          product_id,
+          region_id,
+          user_id,
+          change_in_stock,
+          type,
+          note
+        ]);
+    
+        return result ? result.affectedRows : 0;
+      };
+
+}
+
+
+
+module.exports= new StockMovementModel();
